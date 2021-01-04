1ea564fa641a64cb1d3c09a93a436569e2908a57
Tennie Charissa <tchariss@ox-f5.msk.21-school.ru>
2021-01-04 13:37:30 +0300

    maybe the end

---
+++ GNL2/final_GNL.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   final_GNL.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchariss <tchariss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/23 11:32:09 by tchariss          #+#    #+#             */
+/*   Updated: 2020/12/17 11:29:16 by tchariss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h> // open
+#include <stdio.h> // printf
+
+#include "get_next_line.h" // - включить созданную библиотеку 
+
+char	*proverka_static_memory(char *static_memory, char **line)
+{
+	char	*next_line;// указ на новую линию
+
+	next_line = NULL;
+	if (static_memory != NULL)
+	{
+		if ((next_line = strchr(static_memory, '\n'))) // если static_memory не пустой
+		{
+			*next_line = '\0';
+			*line = strdup(static_memory);
+			next_line++;
+			strcopy(static_memory, next_line); // сдвиг (проспускаем \n)
+		}
+		else
+		{
+			*line = strdup(static_memory);
+			strclear(static_memory);
+		}
+	}
+	else
+		*line = strdup("");
+	return (next_line);
+}
+
+int 	get_next_line(int fd, char **line)
+{
+	char		buf[BUFFER_SIZE + 1]; 	
+	int			count_sym_in_str; 	
+	static char	*static_memory; // остаток, который после '\0'
+	char 		*ukaz_n;
+	//int			stop; // - для остановки цикла, как флаг, 1 - true, 0 - останавливает  цикл (false)
+	char		*saveper; // временная переменная
+	
+	//stop = 1; // true
+	ukaz_n = proverka_static_memory(static_memory, line);
+	// if (static_memory != NULL)
+	// 	*line = strdup(static_memory);
+	// else 
+	// 	*line = strdup(""); 	
+	while (ukaz_n == 0 && (count_sym_in_str = read(fd, buf, BUFFER_SIZE)) > 0) 		
+	{
+		buf[count_sym_in_str] = '\0';
+		if ((ukaz_n = strchr(buf, '\n')) != NULL)
+		{
+			*ukaz_n = '\0'; 						
+		//	stop = 0; // остановка цикла
+			ukaz_n++;
+			static_memory = strdup(ukaz_n);
+		}
+		saveper = *line; // сохраним старое значение line
+		*line = strjoin(*line, buf);
+		free(saveper);
+	}
+	// if ((count_sym_in_str != 0) || (static_memory) != 0) || (strlen(*line) != 0))
+	if (strlen(static_memory) || ukaz_n)
+		return (1);
+	return (0);
+}
+
+int main(void)
+{
+    int fd;
+    char **line;
+    int ret;
+
+    fd = open("fox.txt", O_RDONLY);
+    if (!(line = (char **)malloc(sizeof(char*) * 10000)))
+        return (0);
+
+    while ((ret = get_next_line(fd, line)) > 0)
+    {
+		printf("|%i| %s\n", ret, *line);
+        free(*line);
+    }
+    printf("|%i| %s\n", ret, *line);
+    free(line);
+    return(0);
+}
+// int main()
+// {
+// 	char *line;
+// 	int fd;
+	
+// 	fd = open("fox.txt", O_RDONLY); // fd(1,2) -> 1 - имя файла, 2 - (флаги) как его будем открывать :
+// 	// O_RDONLY - только для чтения, O_WRONLY - только для записи, O_RDWR - для чтения и записи
+// 	get_next_line(fd, &line);
+// 	printf("%s\n", line);
+// 	get_next_line(fd, &line); // 3 строка
+// 	printf("%s\n", line);
+// 	get_next_line(fd, &line); // 4 строка
+// 	printf("%s\n", line);
+// }
---
+++ GNL2/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchariss <tchariss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/30 14:14:26 by tchariss          #+#    #+#             */
+/*   Updated: 2020/12/17 11:35:44 by tchariss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H	 // проверяем было ли уже включено GNL (Нет, не было -> значить включить) то есть повторные включения буду исключены
+# define GET_NEXT_LINE_H	// присвоить имя // имена макросов имеют только заглавные буквы
+# define BUFFER_SIZE 32
+# include <unistd.h>	// отступы после if, ifdef, ifndef + read
+# include <stdlib.h> // malloc 
+# include <string.h> 
+
+int		get_next_line(int fd, char **line); // Ф-ия GNL записывает в *line считанную из fd строку до первого символа \n
+char	*strdup(const char *s); // Дублирует строку, на кот указывает S
+size_t	strlen(const char *s); // Возвращает длину строки
+char	*strjoin(char const *s1, char const *s2); // Объединяет строки в "свежую" строку, заканчивающуюся на \0,
+char	*ft_strchr(const char *s, int c);// Ищет первое вхождения символа(с) в *s
+char	*strcopy(char *s1, const char *s2);
+void	strclear(char *s);
+
+#endif
